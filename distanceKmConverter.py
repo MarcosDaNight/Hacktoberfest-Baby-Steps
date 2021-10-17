66d01797047170c0d635d424e11c2c823b165915
Donato Cutro <donato.cutro3@gmail.com>
2021-10-17 02:20:10 +0200

    Added km converter

---
+++ distanceKmConverter.py
@@ -0,0 +1,14 @@
+print('Enter a distance (in km) : ')
+km = float(input(''))
+m = km * 1000      
+feet = km * 3280.84  
+inch = km * 39370.1  
+cm = km * 100000
+yard = km * 1093.61
+
+print("Distance in km = ", km)
+print("Distance in m = ", m)
+print("Distance in ft = ", feet)
+print("Distance in inches = ", inch)
+print("Distance in cm = ", cm)
+print("Distance in yards = ", yard)
