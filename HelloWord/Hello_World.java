c65683b3bb58c15193592fc202471c583d860d15
ineedhelp-debug <82600879+ineedhelp-debug@users.noreply.github.com>
2021-10-18 18:31:05 -0700

    Update Hello_World.java

--- HelloWord/Hello_World.java
+++ HelloWord/Hello_World.java
@@ -1,7 +1,11 @@
-import java.util.*;
-
-public class Solution{
-  public static void main(String args[]){
-    System.out.println("Hello World!");
-  }
+/* This is a simple Java program.
+   FileName : "HelloWorld.java". */
+class HelloWorld
+{
+    // Your program begins with a call to main().
+    // Prints "Hello, World" to the terminal window.
+    public static void main(String args[])
+    {
+        System.out.println("Hello, World");
+    }
 }
